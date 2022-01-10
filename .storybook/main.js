c001ea765c9345f24d4a8e33bd8763cde7b9ed6f
Edwin <edwinpgm@gmail.com>
2022-01-10 06:00:21 -0500

    feat: add button dropdown

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,24 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          // When you have splitted your css over multiple files
+          // and use @import('./other-styles.css')
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          // When using postCSS 8
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
   framework: '@storybook/react',
   webpackFinal: async (config) => {
     config.resolve.plugins.push(new TsconfigPathsPlugin());
