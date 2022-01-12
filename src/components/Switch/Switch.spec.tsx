735353000b82811cd816d2eec24d4d006e02decc
Edwin Gonzales <edwinpgm@gmail.com>
2022-01-12 06:14:14 -0500

    Switch component (#8)
    * feat: implementation of the Switch component
    * fix: import Switch component

---
+++ src/components/Switch/Switch.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Switch } from './Switch';
+
+describe('Switch', () => {
+  test('renders the Switch component', () => {
+    render(<Switch onChange={jest.fn} />);
+  });
+
+  test('renders the Switch component with checked true', () => {
+    render(<Switch onChange={jest.fn} checked={true} />);
+  });
+});
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './Button';
 export * from './Checkbox';
 export * from './Dropdown';
+export * from './Switch';
 export * from './Table';
 export * from './Tab';
