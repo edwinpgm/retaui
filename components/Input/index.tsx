55a7c9e2efb6e631808d6044c59c8cc254b3d46d
Edwin <edwinpgm@gmail.com>
2022-08-23 06:05:53 -0500

    feat: add label in Input and Textarea components

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import clsx from 'clsx';
 
-export type InputProps = React.ComponentPropsWithoutRef<'input'>;
+export type InputProps = {
+  label?: string;
+  classNameInput?: string;
+} & React.ComponentPropsWithoutRef<'input'>;
 
-export const Input: React.FC<InputProps> = ({ className, ...restProps }) => {
+export const Input: React.FC<InputProps> = ({
+  className,
+  classNameInput,
+  label,
+  ...restProps
+}) => {
   return (
-    <input
-      className={clsx(
-        'px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md text-sm focus:ring-1',
-        className,
+    <div className={className}>
+      {label && (
+        <label className="mb-1 inline-block text-sm font-semibold">
+          {label}
+        </label>
       )}
-      {...restProps}
-    />
+      <input
+        className={clsx(
+          'px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md text-sm focus:ring-1',
+          classNameInput,
+        )}
+        {...restProps}
+      />
+    </div>
   );
 };
--- components/Textarea/index.tsx
+++ components/Textarea/index.tsx
@@ -5,25 +5,34 @@ export interface TextareaProps
   extends React.ComponentPropsWithoutRef<'textarea'> {
   rows?: number;
   className?: string;
+  label?: string;
 }
 
 export const Textarea: React.FC<TextareaProps> = ({
   children,
   rows = 5,
   className,
+  label,
   ...restProps
 }) => {
   return (
-    <textarea
-      rows={rows}
-      className={clsx(
-        'px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md text-sm',
-        'focus:ring-1 focus:outline-none focus:border-sky-500 focus:ring-sky-500',
-        className,
+    <div>
+      {label && (
+        <label className="mb-1 inline-block text-sm font-semibold">
+          {label}
+        </label>
       )}
-      {...restProps}
-    >
-      {children}
-    </textarea>
+      <textarea
+        rows={rows}
+        className={clsx(
+          'px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md text-sm',
+          'focus:ring-1 focus:outline-none focus:border-sky-500 focus:ring-sky-500',
+          className,
+        )}
+        {...restProps}
+      >
+        {children}
+      </textarea>
+    </div>
   );
 };
